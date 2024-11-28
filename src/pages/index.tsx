a171d3c06760368c2a17633e39c96e9fac981f3d
leelongxi <leelongxi@foxmail.com>
2024-11-28 16:47:47 +0800

    Init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
-import { Flex, Heading, IconButton, Text, useColorMode } from '@chakra-ui/react'
+/*
+ * @Author: leelongxi leelongxi@foxmail.com
+ * @Date: 2024-11-28 14:53:26
+ * @LastEditors: leelongxi leelongxi@foxmail.com
+ * @LastEditTime: 2024-11-28 16:46:42
+ * @FilePath: /next-chakra-rainbow-template/src/pages/index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+import { Flex, Heading, IconButton, Text, useColorMode, Button } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 import Head from 'next/head'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useRouter } from 'next/router';
+
 
 export default function Home() {
   const { colorMode, toggleColorMode } = useColorMode()
+  const router = useRouter();
   const isDarkMode = colorMode === 'dark'
+
+  function handleRoute(pathName: string) {
+    router.push(pathName)
+  }
+
   return (
     <>
       <Head>
@@ -37,6 +54,9 @@ export default function Home() {
             aria-label="Toggle color mode"
           />
           <ConnectButton />
+          <Button onClick={handleRoute.bind(null, '/asset')}>资产</Button>
+          <Button onClick={handleRoute.bind(null, '/task')}>任务</Button>
+          <Button onClick={handleRoute.bind(null, '/shareholder')}>股东</Button>
         </Flex>
       </main>
     </>
---
+++ src/pages/task.tsx
@@ -0,0 +1,12 @@
+/*
+ * @Author: leelongxi leelongxi@foxmail.com
+ * @Date: 2024-11-28 16:35:21
+ * @LastEditors: leelongxi leelongxi@foxmail.com
+ * @LastEditTime: 2024-11-28 16:47:22
+ * @FilePath: /next-chakra-rainbow-template/src/pages/task.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+export default function Index() {
+    return '我是任务页面'
+}
